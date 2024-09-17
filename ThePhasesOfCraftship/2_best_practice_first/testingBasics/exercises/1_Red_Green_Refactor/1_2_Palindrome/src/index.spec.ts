ea3302860f058f8b5b8b433e09771a0b7ab57d11
Mario Banovac <mario@tahoma.hr>
2024-09-17 11:05:38 +0200

    [refactor]: should be able to detect that a string is not a palindrome

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
@@ -1,13 +1,13 @@
 import PalindromeChecker from "."
 
 describe('palindrome checker', () => {
-  test.each([['eye', true], ['mom', true], ['wow', true]])('should be able to detect that a %s is a palindrome', (input, expected) => {
+  test.each([['eye', true], ['mom', true], ['wow', true]])('should be able to detect that a %s is a palindrome', (word, expected) => {
     const palindromeChecker = new PalindromeChecker()
-    expect(palindromeChecker.isAPalindrome(input)).toBe(expected)
+    expect(palindromeChecker.isAPalindrome(word)).toBe(expected)
   })
   
-  test('should be able to detect that a string is not a palindrome', () => {
+  test.each([['bill', false], ['car', false], ['code', false]])('should be able to detect that %s is not a palindrome', (word, expected) => {
     const palindromeChecker = new PalindromeChecker()
-    expect(palindromeChecker.isAPalindrome('bill')).toBe(false)
+    expect(palindromeChecker.isAPalindrome(word)).toBe(expected)
   })
 })
