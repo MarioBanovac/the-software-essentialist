a2b8421cd937cf70dad362c724ae97b2944fa613
Mario Banovac <mario@tahoma.hr>
2024-09-17 09:32:16 +0200

    [red]: 3 returns Fizz

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -5,4 +5,8 @@ describe("fizzbuzz", () => {
   test("each return value is a string", () => {
     expect(typeof fizzBuzz()).toBe("string")
   })
+  
+  test("for input 3 the output is Fizz", () => {
+    expect(fizzBuzz(3)).toBe("Fizz")
+  })
 });
