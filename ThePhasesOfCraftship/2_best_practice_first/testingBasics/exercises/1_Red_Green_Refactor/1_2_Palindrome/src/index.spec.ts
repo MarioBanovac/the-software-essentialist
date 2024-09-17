0acf785411e88f98fb52e64462116ff527ec4c71
Mario Banovac <mario@tahoma.hr>
2024-09-17 11:11:13 +0200

    [refactor]: add setup code for tests to a beforeEach

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
@@ -1,18 +1,31 @@
-import PalindromeChecker from "."
+import PalindromeChecker from ".";
 
-describe('palindrome checker', () => {
-  test.each([['eye', true], ['mom', true], ['wow', true]])('should be able to detect that a %s is a palindrome', (word, expected) => {
-    const palindromeChecker = new PalindromeChecker()
-    expect(palindromeChecker.isAPalindrome(word)).toBe(expected)
-  })
-  
-  test.each([['bill', false], ['car', false], ['code', false]])('should be able to detect that %s is not a palindrome', (word, expected) => {
-    const palindromeChecker = new PalindromeChecker()
-    expect(palindromeChecker.isAPalindrome(word)).toBe(expected)
-  })
-  
-  test('should be able to detect palindromes no matter the casing', () => { 
-    const palindromeChecker = new PalindromeChecker()
-    expect(palindromeChecker.isAPalindrome('Mom')).toBe(true)
-  })
-})
+describe("palindrome checker", () => {
+  let palindromeChecker: PalindromeChecker;
+  beforeEach(() => {
+    palindromeChecker = new PalindromeChecker();
+  });
+
+  test.each([
+    ["eye", true],
+    ["mom", true],
+    ["wow", true],
+  ])("should be able to detect that a %s is a palindrome", (word, expected) => {
+    expect(palindromeChecker.isAPalindrome(word)).toBe(expected);
+  });
+
+  test.each([
+    ["bill", false],
+    ["car", false],
+    ["code", false],
+  ])(
+    "should be able to detect that %s is not a palindrome",
+    (word, expected) => {
+      expect(palindromeChecker.isAPalindrome(word)).toBe(expected);
+    }
+  );
+
+  test("should be able to detect palindromes no matter the casing", () => {
+    expect(palindromeChecker.isAPalindrome("Mom")).toBe(true);
+  });
+});
