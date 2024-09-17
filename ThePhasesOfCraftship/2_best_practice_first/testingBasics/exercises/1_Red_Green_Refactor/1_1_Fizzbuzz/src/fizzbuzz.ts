010de47d5ce2b99e7ab4ab7bc227bf0fa6e87938
Mario Banovac <mario@tahoma.hr>
2024-09-17 09:37:04 +0200

    [green]: 15 returns FizzBuzz

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -1,5 +1,6 @@
 export default function fizzBuzz(input: number): string {
   if (input === 3) return "Fizz";
   if (input === 5) return "Buzz";
+  if( input === 15) return "FizzBuzz"
   return "";
 }
