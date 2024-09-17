fa740af5a1bba22e40d1a81ffb9c2a7d4f942796
Mario Banovac <mario@tahoma.hr>
2024-09-17 09:56:11 +0200

    [green]: 101 throws an error

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -1,4 +1,6 @@
 export default function fizzBuzz(input: number): string {
+  if(input > 100) throw new Error('The number is too big')
+  
   if (input % 15 === 0) return "FizzBuzz";
   if (input % 5 === 0) return "Buzz";
   if (input % 3 === 0) return "Fizz";
