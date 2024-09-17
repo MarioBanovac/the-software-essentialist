1574315317a2e8515d3e4ad4abc438aea192e408
Mario Banovac <mario@tahoma.hr>
2024-09-17 11:09:24 +0200

    [refactor]: add getReverseWord to the PalindromeChecker

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_2_Palindrome/src/index.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_2_Palindrome/src/index.ts
@@ -1,10 +1,16 @@
 export interface IPalindromeChecker {
   isAPalindrome: (word: string) => boolean
+  getReverseWord: (word: string) => string
 }
 
 export default class PalindromeChecker implements IPalindromeChecker {
+  
+  getReverseWord(word: string): string {
+    return word.split('').reverse().join('')
+  }
+  
   isAPalindrome(word: string) {
-    const reverseWord = word.split('').reverse().join('').toLowerCase()
+    const reverseWord = this.getReverseWord(word).toLowerCase()
     return word.toLowerCase() == reverseWord
   }
 }
