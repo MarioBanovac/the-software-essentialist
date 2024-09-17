5a90dec601f6c15fce631dd1c7d76d86ff7826d6
Mario Banovac <mario@tahoma.hr>
2024-09-17 13:29:12 +0200

    [refactor]: remove unecessary arrays in test definitions

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -5,22 +5,25 @@ describe("fizzbuzz", () => {
     expect(typeof fizzBuzz(3)).toBe("string");
   });
 
-  test.each([[3], [9], [42]])("for input %i the output is Fizz", (input) => {
+  test.each([3, 9, 42])("for input %i the output is Fizz", (input) => {
     expect(fizzBuzz(input)).toBe("Fizz");
   });
-  
-  test.each([[5], [20], [40]])("for input %i the output is Buzz", (input) => {
+
+  test.each([5, 20, 40])("for input %i the output is Buzz", (input) => {
     expect(fizzBuzz(input)).toBe("Buzz");
   });
-  
-  test.each([[15], [45], [90]])("for input %i the output is FizzBuzz", (input) => {
+
+  test.each([15, 45, 90])("for input %i the output is FizzBuzz", (input) => {
     expect(fizzBuzz(input)).toBe("FizzBuzz");
   });
-  
-  test.each([[2, "2"], [17, "17"], [26, "26"]])("for input %i the output is %p", (input) => {
+
+  test.each([
+    [2, "2"],
+    [17, "17"],
+    [26, "26"],
+  ])("for input %i the output is %p", (input) => {
     expect(fizzBuzz(input)).toBe(input.toString());
   });
-
 
   test("for numbers larger than 100 an error is thrown", () => {
     expect(() => fizzBuzz(101)).toThrowError("The number is too big");
