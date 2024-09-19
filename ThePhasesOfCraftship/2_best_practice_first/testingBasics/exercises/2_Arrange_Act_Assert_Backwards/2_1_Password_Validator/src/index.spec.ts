1cf696bb7582b7a655f13c2c75f4a37fcee4717e
Mario Banovac <mario@tahoma.hr>
2024-09-19 11:16:01 +0200

    [refactor-2]: knows when the password is between 5 and 15 characters long

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.spec.ts
@@ -1,9 +1,9 @@
 import PasswordValidator from "."
 
 describe('password validator', () => {
-  test.each(['password', '12345', '#$%&/'])('knows that the password: %s is between 5 and 15 characters long', () => { 
+  test.each(['password', '12345', '#$%&/'])('knows that the password: %s is between 5 and 15 characters long', (userInput) => { 
     let passwordValidator = new PasswordValidator()
-    expect(passwordValidator.isValid('password')).toEqual({
+    expect(passwordValidator.isValid(userInput)).toEqual({
       success: true
     })
   })
