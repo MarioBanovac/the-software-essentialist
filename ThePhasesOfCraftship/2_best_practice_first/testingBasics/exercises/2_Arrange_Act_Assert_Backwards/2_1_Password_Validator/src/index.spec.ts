dce283335a068b63003f7531f5d1b5393fd3c51d
Mario Banovac <mario@tahoma.hr>
2024-09-19 11:12:43 +0200

    [refactor]: knows when the password is between 5 and 15 characters long

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.spec.ts
@@ -1,7 +1,7 @@
 import PasswordValidator from "."
 
 describe('password validator', () => {
-  test('knows if the password is between 5 and 15 characters long', () => { 
+  test.each(['password', '12345', '#$%&/'])('knows that the password: %s is between 5 and 15 characters long', () => { 
     let passwordValidator = new PasswordValidator()
     expect(passwordValidator.isValid('password')).toEqual({
       success: true
--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.ts
@@ -1,7 +1,15 @@
 export default class PasswordValidator {
+  private hasValidLength(userInput: string) {
+    const isLengthValid = userInput.length >= 5 && userInput.length <= 15;
+
+    return isLengthValid;
+  }
+
   isValid(userInput: string) {
-    return {
-      success: true
+    if (this.hasValidLength(userInput)) {
+      return {
+        success: true,
+      };
     }
   }
 }
