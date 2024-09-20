c80df6058317779f1d413c3266c441b8a30aa3ec
Mario Banovac <mario@tahoma.hr>
2024-09-20 09:42:15 +0200

    [refactor]: test each business rule as oppossed to everything at once

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.spec.ts
@@ -3,17 +3,12 @@ import PasswordValidator from "."
 describe('password validator', () => {
   test.each(['password', '12345', '#$%&/'])('knows that the password: %s is between 5 and 15 characters long', (userInput) => { 
     let passwordValidator = new PasswordValidator()
-    expect(passwordValidator.isValid(userInput)).toEqual({
-      success: true
-    })
+    expect(passwordValidator.hasValidLength(userInput)).toBeTruthy()
   })
   
   test.each(['pass', 'password12345678'])('knows that the password: %s is not between 5 and 15 characters long', (userInput) => {
     let passwordValidator = new PasswordValidator()
-    expect(passwordValidator.isValid(userInput)).toEqual({
-      success: false,
-      error: ['password is too short']
-    })
+    expect(passwordValidator.hasValidLength(userInput)).toBeFalsy()
   })
 })
 
--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.ts
@@ -1,5 +1,5 @@
 export default class PasswordValidator {
-  private hasValidLength(userInput: string) {
+   hasValidLength(userInput: string) {
     const isLengthValid = userInput.length >= 5 && userInput.length <= 15;
 
     return isLengthValid;
