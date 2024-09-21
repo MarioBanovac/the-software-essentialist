e743e8cc13e51f7ac9122cd869307c54971eb79e
Mario Banovac <mario@tahoma.hr>
2024-09-21 11:34:05 +0200

    [refactor]: add paramaterization to the tests

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.spec.ts
@@ -4,34 +4,39 @@ import StatsCalculator, { IStats } from "."
 describe('stats calculator', () => {
   let statsCalculator: StatsCalculator
   let inputStats: IStats | undefined
+  const TEST_DATA = [
+    [
+      [2,4,21,-8,53,40]
+    ]
+  ]
   beforeEach(() => {
     statsCalculator = new StatsCalculator()
     inputStats = undefined
   })
   
-  test('knows how to find the minimum value', () => {
-    inputStats =  statsCalculator.getStats([2,4,21,-8,53,40])
+  test.each(TEST_DATA)('knows how to find the minimum value', (input) => {
+    inputStats =  statsCalculator.getStats(input)
     
     expect(inputStats).toHaveProperty('minimumValue')
     expect(inputStats.minimumValue).toBe(-8)
   })
   
-  test('knows how to find the maximum value', () => {
-    inputStats = statsCalculator.getStats([2,4,21,-8,53,40])
+  test.each(TEST_DATA)('knows how to find the maximum value', (input) => {
+    inputStats = statsCalculator.getStats(input)
     
     expect(inputStats).toHaveProperty('maximumValue')
     expect(inputStats.maximumValue).toBe(53)
   })
   
-  test('knows how to find the number of elements', () => {
-    inputStats = statsCalculator.getStats([2,4,21,-8,53,40])
+  test.each(TEST_DATA)('knows how to find the number of elements', (input) => {
+    inputStats = statsCalculator.getStats(input)
     
     expect(inputStats).toHaveProperty('size')
     expect(inputStats.size).toBe(6)
   })
   
-  test('knows how to find the average value', () => {
-    inputStats = statsCalculator.getStats([2,4,21,-8,53,40])
+  test.each(TEST_DATA)('knows how to find the average value', (input) => {
+    inputStats = statsCalculator.getStats(input)
     
     expect(inputStats).toHaveProperty('averageValue')
     expect(inputStats.averageValue).toBe(18)
