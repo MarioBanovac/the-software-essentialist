ad302d0d70382f4a0292bfa6134d36493618090d
Mario Banovac <mario@tahoma.hr>
2024-09-21 11:13:08 +0200

    [refactor]: implement getMinimumValue, getMaximumValue private methods and refactored the test code

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.spec.ts
@@ -1,31 +1,30 @@
 
-import StatsCalculator from "."
+import StatsCalculator, { IStats } from "."
 
 describe('stats calculator', () => {
+  let statsCalculator: StatsCalculator
+  let inputStats: IStats | undefined
+  beforeEach(() => {
+    statsCalculator = new StatsCalculator()
+    inputStats = undefined
+  })
+  
   test('knows how to find the minimum value', () => {
-    const statsCalculator = new StatsCalculator()
-    
-    const inputStats = statsCalculator.getStats([2,4,21,-8,53,40])
+    inputStats =  statsCalculator.getStats([2,4,21,-8,53,40])
     
     expect(inputStats).toHaveProperty('minimumValue')
     expect(inputStats.minimumValue).toBe(-8)
   })
   
   test('knows how to find the maximum value', () => {
-    
-    const statsCalculator = new StatsCalculator()
-    
-    const inputStats = statsCalculator.getStats([2,4,21,-8,53,40])
+    inputStats = statsCalculator.getStats([2,4,21,-8,53,40])
     
     expect(inputStats).toHaveProperty('maximumValue')
     expect(inputStats.maximumValue).toBe(53)
   })
   
   test('knows how to find the number of elements', () => {
-    
-    const statsCalculator = new StatsCalculator()
-    
-    const inputStats = statsCalculator.getStats([2,4,21,-8,53,40])
+    inputStats = statsCalculator.getStats([2,4,21,-8,53,40])
     
     expect(inputStats).toHaveProperty('size')
     expect(inputStats.size).toBe(6)
--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.ts
@@ -1,9 +1,36 @@
+export interface IStats {
+  minimumValue: number,
+  maximumValue: number,
+  size: number
+}
+
 export default class StatsCalculator {
-  public getStats(input: number[]) {
+  
+  private getMinimumValue(input: number[]): number {
+    let minimumValue = input[0]
+    for(let i = 1; i < input.length; i++) {
+      if(input[i] < minimumValue) {
+        minimumValue = input[i]
+      }
+    }
+    return minimumValue
+  }
+  
+  private getMaximumValue(input: number[]): number {
+    let maximumValue = input[0]
+    for(let i = 1; i < input.length; i++) {
+      if(input[i] > maximumValue) {
+        maximumValue = input[i]
+      }
+    }
+    return maximumValue
+  }
+  
+  public getStats(input: number[]): IStats {
     return {
-      minimumValue: -8,
-      maximumValue: 53,
-      size: 6
+      minimumValue: this.getMinimumValue(input),
+      maximumValue: this.getMaximumValue(input),
+      size: input.length
     }
   }
 }
