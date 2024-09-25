828a303c7f76ee45b8d7d8f21070f696047067fb
Mario Banovac <mario@tahoma.hr>
2024-09-25 09:37:16 +0200

    [refactor]: add extractStartHour method

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
@@ -1,7 +1,11 @@
 export default class MilitaryTimeValidator {
   public isValidRange(time: string): boolean {
-    const startHour = parseInt(time.split('-')[0].split(':')[0])
+    const startHour = this.extractStartHour(time)
     const endHour = parseInt(time.split('-')[1].split(':')[0])
     return startHour >= 0 && startHour < 24 && endHour >= 0 && endHour < 24
+  }
+  
+  private extractStartHour(time: string): number {
+    return parseInt(time.split('-')[0].split(':')[0])
   }
 }
