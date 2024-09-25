e65e1d9be0719024cba889311f4f5b73263e1010
Mario Banovac <mario@tahoma.hr>
2024-09-25 09:14:30 +0200

    [green]: military time validator should be defined

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
@@ -1,5 +1,8 @@
+import MilitaryTimeValidator from "."
 
 describe('military time validator', () => {
-
-
+  test('should be defined', () => { 
+    let militaryTimeValidator = new MilitaryTimeValidator()
+    expect(militaryTimeValidator).toBeDefined()
+  })
 })
