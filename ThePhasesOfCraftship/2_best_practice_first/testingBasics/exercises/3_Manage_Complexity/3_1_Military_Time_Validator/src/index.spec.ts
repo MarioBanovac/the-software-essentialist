8208f662a7a37a957428a65930b3d2b70ab1e885
Mario Banovac <mario@tahoma.hr>
2024-09-25 09:29:29 +0200

    [refacator]: added beforeEach to the test suite to remove duplication

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
@@ -1,17 +1,19 @@
 import MilitaryTimeValidator from "."
 
 describe('military time validator', () => {
+  let militaryTimeValidator: MilitaryTimeValidator
+  beforeEach(() => {
+    militaryTimeValidator = new MilitaryTimeValidator()
+  })
+  
   test('should be defined', () => { 
-    let militaryTimeValidator = new MilitaryTimeValidator()
     expect(militaryTimeValidator).toBeDefined()
   })
   
   test('should know that 00:00 - 23:59 is a valid military time', () => {
-    let militaryTimeValidator = new MilitaryTimeValidator()
     expect(militaryTimeValidator.isValidRange('00:00 - 23:59')).toBeTruthy()
   })
   test('should know that 25:00 - 01:00 is not a valid military time', () => {
-    let militaryTimeValidator = new MilitaryTimeValidator()
     expect(militaryTimeValidator.isValidRange('25:00 - 01:00')).toBeFalsy()
   })
 })
