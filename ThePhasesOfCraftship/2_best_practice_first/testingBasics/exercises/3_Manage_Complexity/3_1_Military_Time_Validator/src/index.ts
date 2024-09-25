490f2591ef7210ccb2620106c9350aed6f1a3299
Mario Banovac <mario@tahoma.hr>
2024-09-25 09:31:59 +0200

    [refacator]: fix minor issue in the code for determining starting hour valid range

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
@@ -1,6 +1,6 @@
 export default class MilitaryTimeValidator {
   public isValidRange(time: string): boolean {
     const startHour = parseInt(time.split('-')[0].split(':')[0])
-    return startHour >= 0 && startHour <= 24
+    return startHour >= 0 && startHour < 24
   }
 }
