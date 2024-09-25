815f4a5b52bd542fa8e8b24af45b013844cefead
Mario Banovac <mario@tahoma.hr>
2024-09-25 09:50:09 +0200

    [refactor]: add .each for valid military times tests

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
@@ -10,14 +10,8 @@ describe('military time validator', () => {
     expect(militaryTimeValidator).toBeDefined()
   })
   
-  test('should know that 00:00 - 23:59 is a valid military time', () => {
-    expect(militaryTimeValidator.isValidRange('00:00 - 23:59')).toBeTruthy()
-  })
-  test('should know that 18:00 - 09:59 is a valid military time', () => {
-    expect(militaryTimeValidator.isValidRange('18:00 - 09:59')).toBeTruthy()
-  })
-  test('should know that 01:01 - 09:10 is a valid military time', () => {
-    expect(militaryTimeValidator.isValidRange('01:01 - 09:10')).toBeTruthy()
+  test.each(['00:00 - 23:59', '18:00 - 09:59', '01:01 - 09:10'])('should know that %s is a valid military time', (time) => {
+    expect(militaryTimeValidator.isValidRange(time)).toBeTruthy()
   })
   test.each(['25:00 - 01:00', '10:45 - 27:15', '23:60 - 13:00', '12:00 - 19:61'])('should know that %s is not a valid military time', (time) => {
     expect(militaryTimeValidator.isValidRange(time)).toBeFalsy()
