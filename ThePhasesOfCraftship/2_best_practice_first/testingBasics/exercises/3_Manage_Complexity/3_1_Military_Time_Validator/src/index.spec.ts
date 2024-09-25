7e982e6106d3336467c74dbca475dfabbb6ea3c0
Mario Banovac <mario@tahoma.hr>
2024-09-25 09:48:15 +0200

    [green]: should know that 01:01 - 09:10 is a valid military time

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
@@ -13,6 +13,9 @@ describe('military time validator', () => {
   test('should know that 00:00 - 23:59 is a valid military time', () => {
     expect(militaryTimeValidator.isValidRange('00:00 - 23:59')).toBeTruthy()
   })
+  test('should know that 01:01 - 09:10 is a valid military time', () => {
+    expect(militaryTimeValidator.isValidRange('01:01 - 09:10')).toBeTruthy()
+  })
   test.each(['25:00 - 01:00', '10:45 - 27:15', '23:60 - 13:00', '12:00 - 19:61'])('should know that %s is not a valid military time', (time) => {
     expect(militaryTimeValidator.isValidRange(time)).toBeFalsy()
   })
