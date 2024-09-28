e5f805eaef45483393260c8f42ab7156a7c0c7ec
Mario Banovac <mario@tahoma.hr>
2024-09-28 21:25:36 +0200

    [refactor]: add beforeEach to the test suite

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_2_Boolean_Calculator/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_2_Boolean_Calculator/src/index.spec.ts
@@ -1,18 +1,21 @@
 import BooleanCalculator from "."
 
 describe('boolean calculator', () => {
+  let booleanCalculator: BooleanCalculator
+  
+  beforeEach(() => {
+    booleanCalculator = new BooleanCalculator()
+  })
+  
   test('knows that "TRUE" should return true', () => { 
-    const booleanCalculator = new BooleanCalculator()
     expect(booleanCalculator.calculate("TRUE")).toBe(true)
   })
   
   test('knows that "FALSE" should return false', () => {
-    const booleanCalculator = new BooleanCalculator()
     expect(booleanCalculator.calculate("FALSE")).toBe(false)
   })
   
   test('knows that "NOT" operator reverts the expression', () => {
-    const booleanCalculator = new BooleanCalculator()
     expect(booleanCalculator.calculate("NOT FALSE")).toBe(true)
     expect(booleanCalculator.calculate("NOT TRUE")).toBe(false)
   })
