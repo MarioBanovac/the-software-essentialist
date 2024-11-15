478c9e8611e6502e3cee9b5a5db24cd6e2def990
Mario Banovac <mario@tahoma.hr>
2024-11-15 14:38:52 +0100

    Add success scenario & acceptance test for getting all existing students

---
+++ ThePhasesOfCraftship/2_best_practice_first/strategicDesignPart2/exercises/1_CharacterizeAndRefactorE2EBackend/begin/tests/acceptance/get_all_students.test.ts
@@ -0,0 +1,47 @@
+import request from "supertest";
+import { defineFeature, loadFeature } from "jest-cucumber";
+import { app } from "../../src/index";
+import path from "path";
+import { resetDatabase } from "../fixtures/reset";
+import studentBuilder from "../builders/studentBuilder";
+
+const feature = loadFeature(
+  path.join(__dirname, "../features/get_all_students.feature")
+);
+
+defineFeature(feature, (test) => {
+  let response: any
+  beforeEach(async () => {
+    await resetDatabase()
+    response = {}
+  })
+  
+  test('Successfully get all students', ({ given, when, then }) => {
+    given('there are total of three existing students', async () => {
+        const randomStudents = [{
+          email: 'gmail@gmail.com',
+          name: 'John'
+        },
+        {
+          email: 'yahoo@yahoo.com',
+          name: 'Jane'
+        },
+        {
+          email: 'proton@protonmail.com',
+          name: 'Joe'
+        }]
+        for (const {name, email} of randomStudents) {
+          await studentBuilder().withName(name).withEmail(email).build()
+        }   
+    });
+
+    when('I request to get all students', async () => {
+       response = await request(app).get('/students')
+    });
+
+    then('I should get all existing students', () => {
+       expect(response.body.data.length).toBe(3)
+       expect(response.body.success).toBeTruthy()
+    });
+});
+})
